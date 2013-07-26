06c6183345f95415ad039735911162827ce5779b
zhangjiajie <bestzhangjiajie@gmail.com>
2013-07-26 15:59:59 +0200

    PTP base

--- sd_server/ptp/urls.py
+++ sd_server/ptp/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from ptp import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^ptp/$', views.ptp_index, name='ptp_index'),
 )
--- sd_server/ptp/views.py
+++ sd_server/ptp/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+
+def ptp_index(request):
+    context = {}
+    return render(request, 'ptp/index.html', context)
